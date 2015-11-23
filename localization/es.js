14fd40fea475953d9dc0ac7f3ad3190b1d3649f2
JaBO <admin@jaboweb.com>
2015-11-23 19:58:36 +0100

    add SPANISH localization

---
+++ localization/es.js
@@ -0,0 +1,55 @@
+/**
+ * jui_filter_rules v1.0.7 simple localization - ENGLISH
+ *
+ * DO NOT CHANGE this file, as it will be overwritten in next update.
+ * To use different values, write and use a similar structure js file.
+ *
+ */
+var rsc_jui_fr = {
+
+    no_filters_found: "Filtro no definido",
+
+    rules_group_AND: "todas las reglas",
+    rules_group_OR: "cualquiera de las reglas",
+
+    rule: "regla",
+    group: "grupo",
+    tools_please_select: "&raquo;",
+    rule_insert_before: "insertar antes",
+    rule_insert_after: "insertar despues",
+    rule_insert_inside: "insertar dentro",
+    rule_clear: "borrar",
+    rule_delete: "eliminar",
+    group_insert_before: "insertar antes",
+    group_insert_after: "insertar despues",
+    group_insert_inside: "insertar dentro",
+    group_delete: "eliminar",
+
+    filter_please_select: '&raquo; seleccionar',
+
+    operator_equal: 'igual',
+    operator_not_equal: 'distinto',
+    operator_in: 'es',
+    operator_not_in: 'no es',
+    operator_less: 'menor',
+    operator_less_or_equal: 'menor o igual',
+    operator_greater: 'mayor',
+    operator_greater_or_equal: 'mayor o igual',
+    operator_between: 'entre',
+    operator_not_between: 'no entre',
+    operator_begins_with: 'comienza por',
+    operator_not_begins_with: 'no comienza por',
+    operator_contains: 'contiene',
+    operator_not_contains: 'no contiene',
+    operator_ends_with: 'termina por',
+    operator_not_ends_with: 'no termina por',
+    operator_is_empty: 'vacio',
+    operator_is_not_empty: 'no vacio',
+    operator_is_null: 'nulo',
+    operator_is_not_null: 'no nulo',
+
+    error_no_value_given: "Valor no introducido",
+    error_invalid_number: "Valor no es numerico",
+    error_invalid_datetime: "Formato de fecha invalido",
+    error_converting_value: "Error al convertir valor"
+};
